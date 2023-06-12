f09580e5f12549fc058bc237aba0b1e3cb26bc3d
Heo Seonjo <heo-seonjo@wemadeonline.co.jp>
2023-06-13 00:26:55 +0900

    요구사항 2 해

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -45,7 +49,18 @@ public class RequestHandler extends Thread {
         		log.debug("Header : {}",line);
         	}
         	
+        	String url = tokens[1];
         	
+        	if (url.startsWith("/user/create")) {
+        		log.debug("Task - /user/create");
+        		int index = url.indexOf("?");
+        		String queryString = url.substring(index+1);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+        		
+        		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        		
+        		log.debug("User : {}", user);
+        	}else {
             DataOutputStream dos = new DataOutputStream(out);
             
             //byte[] body = "Hello World".getBytes();
@@ -54,6 +69,7 @@ public class RequestHandler extends Thread {
             byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+        	}
         } catch (IOException e) {
             log.error(e.getMessage());
         }
